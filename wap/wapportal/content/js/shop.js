52aac2784a911b9165ff5e69967536e19ef96719
wenyuzeng <zdamao@126.com>
2017-12-10 16:33:14 +0800

    rename

--- wap/wapportal/content/js/shop.js
+++ wap/wapportal/content/js/shop.js
@@ -1,7 +1,22 @@
 $(function(){
+    //panel-shop-switcher
     $(".panel-heading").click(function(e){
-        /*切换折叠指示图标*/
+        // switch arrow indicator
         $(this).find("span").toggleClass("glyphicon-chevron-up");
         $(this).find("span").toggleClass("glyphicon-chevron-down");
+    });
+    $(".list-group-item > a").click(function (e) {
+        var $curTitle = $("#panel-shop-heading > h4 > #panel-title-txt");
+        $('.panel-collapse').collapse('toggle');
+        // change title according to list item clicked
+        if (e.target == this ) {
+            $(".panel-footer").removeClass("panel-footer");
+            $(this).parents(".list-group-item").addClass("panel-footer");
+            var $newTitle = $(this).text();
+            // alert();
+            $curTitle.text($newTitle);
+        }
+
+        e.preventDefault();
     });
 });
