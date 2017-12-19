9b22cf3241b4278828ff8343567d823df1435b87
wenyuzeng <zdamao@126.com>
2017-12-19 09:19:46 +0800

    adjust page1 position

--- wap/wapportal/content/js/worker.js
+++ wap/wapportal/content/js/worker.js
@@ -61,3 +61,140 @@ $(document).ready(function () {
         }
     })
 });
+
+$(document).ready(function () {
+    // var num = ($(".num").text().substring(0,3));
+    // console.log(num);
+    $(".rateYo-50").rateYo({
+        rating: 5.0,
+        maxValue: 5,
+        numStars: 5,
+        precision: 2,
+        starWidth: "14px",
+        spacing: "0",
+        normalFill: "#AEAEAE",
+        ratedFill: "#e8314a",
+        readOnly: true
+    });
+    $(".rateYo-49").rateYo({
+        rating: 4.9,
+        maxValue: 5,
+        numStars: 5,
+        precision: 2,
+        starWidth: "14px",
+        spacing: "0",
+        normalFill: "#AEAEAE",
+        ratedFill: "#e8314a",
+        readOnly: true
+    });
+    $(".rateYo-48").rateYo({
+        rating: 4.8,
+        maxValue: 5,
+        numStars: 5,
+        precision: 2,
+        starWidth: "14px",
+        spacing: "0",
+        normalFill: "#AEAEAE",
+        ratedFill: "#e8314a",
+        readOnly: true
+    });
+    $(".rateYo-47").rateYo({
+        rating: 4.7,
+        maxValue: 5,
+        numStars: 5,
+        precision: 2,
+        starWidth: "14px",
+        spacing: "0",
+        normalFill: "#AEAEAE",
+        ratedFill: "#e8314a",
+        readOnly: true
+    });
+    $(".rateYo-46").rateYo({
+        rating: 4.6,
+        maxValue: 5,
+        numStars: 5,
+        precision: 2,
+        starWidth: "14px",
+        spacing: "0",
+        normalFill: "#AEAEAE",
+        ratedFill: "#e8314a",
+        readOnly: true
+    });
+    $(".rateYo-45").rateYo({
+        rating: 4.5,
+        maxValue: 5,
+        numStars: 5,
+        precision: 2,
+        starWidth: "14px",
+        spacing: "0",
+        normalFill: "#AEAEAE",
+        ratedFill: "#e8314a",
+        readOnly: true
+    });
+    $(".rateYo-50-sm").rateYo({
+        rating: 5.0,
+        maxValue: 5,
+        numStars: 5,
+        precision: 2,
+        starWidth: "14px",
+        spacing: "0",
+        normalFill: "#AEAEAE",
+        ratedFill: "#e8314a",
+        readOnly: true
+    });
+    $(".rateYo-49-sm").rateYo({
+        rating: 4.9,
+        maxValue: 5,
+        numStars: 5,
+        precision: 2,
+        starWidth: "14px",
+        spacing: "0",
+        normalFill: "#AEAEAE",
+        ratedFill: "#e8314a",
+        readOnly: true
+    });
+    $(".rateYo-48-sm").rateYo({
+        rating: 4.8,
+        maxValue: 5,
+        numStars: 5,
+        precision: 2,
+        starWidth: "14px",
+        spacing: "0",
+        normalFill: "#AEAEAE",
+        ratedFill: "#e8314a",
+        readOnly: true
+    });
+    $(".rateYo-47-sm").rateYo({
+        rating: 4.7,
+        maxValue: 5,
+        numStars: 5,
+        precision: 2,
+        starWidth: "14px",
+        spacing: "0",
+        normalFill: "#AEAEAE",
+        ratedFill: "#e8314a",
+        readOnly: true
+    });
+    $(".rateYo-46-sm").rateYo({
+        rating: 4.6,
+        maxValue: 5,
+        numStars: 5,
+        precision: 2,
+        starWidth: "14px",
+        spacing: "0",
+        normalFill: "#AEAEAE",
+        ratedFill: "#e8314a",
+        readOnly: true
+    });
+    $(".rateYo-45-sm").rateYo({
+        rating: 4.5,
+        maxValue: 5,
+        numStars: 5,
+        precision: 2,
+        starWidth: "14px",
+        spacing: "0",
+        normalFill: "#AEAEAE",
+        ratedFill: "#e8314a",
+        readOnly: true
+    });
+})
