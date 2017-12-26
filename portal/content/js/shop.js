8e9e388b9fb5578ac74f55b3abed0f70ac712fd7
wenyuzeng <zdamao@126.com>
2017-12-26 22:36:46 +0800

    add brand wp page

--- portal/content/js/shop.js
+++ portal/content/js/shop.js
@@ -1,25 +1,25 @@
 $(function(){
-    //panel-shop-switcher
-    $(".panel-heading").click(function(e){
-        // switch arrow indicator
-        $(this).find("span").toggleClass("glyphicon-chevron-up");
-        $(this).find("span").toggleClass("glyphicon-chevron-down");
-    });
-    $(".list-group-item > a").click(function (e) {
-
-        var $curTitle = $("#panel-shop-heading > h4 > #panel-title-txt");
-        $('.panel-collapse').collapse('toggle');
-        // change title according to list item clicked
-        if (e.target == this ) {
-            $(".panel-footer").removeClass("panel-footer");
-            $(this).parents(".list-group-item").addClass("panel-footer");
-            var $newTitle = $(this).text();
-            // alert();
-            $curTitle.text($newTitle);
-        }
-
-        e.preventDefault();
-    });
+    // //panel-shop-switcher
+    // $(".panel-heading").click(function(e){
+    //     // switch arrow indicator
+    //     $(this).find("span").toggleClass("glyphicon-chevron-up");
+    //     $(this).find("span").toggleClass("glyphicon-chevron-down");
+    // });
+    // $(".list-group-item > a").click(function (e) {
+    //
+    //     var $curTitle = $("#panel-shop-heading > h4 > #panel-title-txt");
+    //     $('.panel-collapse').collapse('toggle');
+    //     // change title according to list item clicked
+    //     if (e.target == this ) {
+    //         $(".panel-footer").removeClass("panel-footer");
+    //         $(this).parents(".list-group-item").addClass("panel-footer");
+    //         var $newTitle = $(this).text();
+    //         // alert();
+    //         $curTitle.text($newTitle);
+    //     }
+    //
+    //     e.preventDefault();
+    // });
 
     // //select list
     // var $curTxt = $(".select > .text");
@@ -45,6 +45,39 @@ $(function(){
 
     //select list
 
+    // var selectPop = function (e) {
+    //     // console.log(e.target.tagName + " is clicked")
+    //     var $self = $(this);
+    //     // console.log($self[0].tagName + " is clicked")
+    //     $self.next(".option").slideToggle("fast");
+    //     $(document).one("click", function(){
+    //         $(".option").slideUp("fast");
+    //     });
+    //
+    //     // set title to chicked list item
+    //
+    //     $(this).next(".option").children("div").click(function (e) {
+    //     // $(this).next(".option > div").click(function (e) {
+    //         var $curTxt = $self.children(".text");
+    //         // var $mbNum = $self.parent().parent().parent().parent().find(".indexMobile").eq(0).val();
+    //         console.log($mbNum);
+    //         // console.log($self + " is clicked")
+    //         if (e.target == this ) {
+    //
+    //             var $newTitle = $(this).text();
+    //             // console.log($newTitle);
+    //             $curTxt.text($newTitle);
+    //             $(".option").slideUp("fast");
+    //         } else {
+    //             $(".option").slideUp("fast");
+    //         };
+    //     })
+    // };
+    //
+    // $(document).on("click", ".select-trigger", selectPop);
+
+    //select list
+
     var selectPop = function (e) {
         // console.log(e.target.tagName + " is clicked")
         var $self = $(this);
@@ -57,7 +90,7 @@ $(function(){
         // set title to chicked list item
 
         $(this).next(".option").children("div").click(function (e) {
-        // $(this).next(".option > div").click(function (e) {
+            // $(this).next(".option > div").click(function (e) {
             var $curTxt = $self.children(".text");
             // console.log($self + " is clicked")
             if (e.target == this ) {
